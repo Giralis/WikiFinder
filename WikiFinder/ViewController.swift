e5a11c26190811be30ad6ce14da902bc7b1f5029
Vladimir Timofeev <vladimirmail895@gmail.com>
2021-11-05 12:51:47 +0300

    Initial Commit

---
+++ WikiFinder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WikiFinder
+//
+//  Created by Владимир Тимофеев on 05.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
